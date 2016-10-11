517b87ed7f64f368bbf21d1c033a011de63a41ac
manyan.chen <xxmajia@gmail.com>
2016-10-11 21:12:28 +0800

    Initial Commit

---
+++ HelloWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by xxmajia on 11/10/16.
+//  Copyright © 2016 xxmajia.org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelloWorld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by xxmajia on 11/10/16.
+//  Copyright © 2016 xxmajia.org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
